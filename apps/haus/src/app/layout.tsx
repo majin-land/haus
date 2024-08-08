284c789333bff797cbe8209f593337f30678c089
Muhammad ilham <muhammad.ilhamsa1@gmail.com>
2024-08-08 22:35:04 +0700

    merge: setup-base

--- apps/haus/src/app/layout.tsx
+++ apps/haus/src/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from 'next'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
+import '@coinbase/onchainkit/styles.css'
+
+import BaseProvider from '@/components/provider/base'
 
 import theme from '@/theme'
 import Header from '@/components/header'
@@ -11,18 +14,22 @@ export const metadata: Metadata = {
   title: 'Haus',
 }
 
+const BASE_KEY = process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY || ''
+
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <Header />
-            {props.children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            <ThemeProvider theme={theme}>
+              <BaseProvider apiKey={BASE_KEY}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Header />
+                {props.children}
+              </BaseProvider>
+            </ThemeProvider>
+          </AppRouterCacheProvider>
       </body>
     </html>
   )
---
+++ apps/haus/src/components/provider/base.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { base } from 'viem/chains'
+
+import { OnchainKitProvider } from '@coinbase/onchainkit'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; 
+import { WagmiProvider } from 'wagmi'; 
+import { wagmiConfig } from './wagmi'; 
+
+const queryClient = new QueryClient(); 
+
+const BaseProvider = ({ apiKey, children }: { apiKey: string, children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <OnchainKitProvider apiKey={apiKey} chain={base}>
+        {children}
+        </OnchainKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider> 
+  )
+
+}
+
+export default BaseProvider
