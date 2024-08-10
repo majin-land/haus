bfbbf2f0676ffb2f8c4233e05b0885edc23d97e6
mrisqid <mrisqid@gmail.com>
2024-08-10 21:04:28 +0700

    feat: added fetching ticket attestation data

--- apps/haus/src/app/ticket/page.tsx
+++ apps/haus/src/app/ticket/page.tsx
@@ -1,7 +1,39 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { useAccount } from 'wagmi'
 import Tickets from '@/components/tickets'
 
 function TicketPage() {
-  return <Tickets />
+  const { address } = useAccount()
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true)
+        const response = await fetch(`/api/attestation?recipient=${address}`)
+        const result = await response.json()
+        setData(result)
+      } catch (error) {
+        console.error('Error fetching attestations:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    if (address) {
+      fetchData()
+    }
+  }, [])
+
+  if (loading) return <>Loading...</>
+  return (
+    <>
+      <Tickets />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  )
 }
 
 export default TicketPage
